e1eeee631a9304d2440ea295f383189d78a57ee6
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-02-10 12:12:09 -0600

    fixed

--- SuperbowlDates/SuperbowlDateTests.cs
+++ SuperbowlDates/SuperbowlDateTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,21 @@ using System.Threading.Tasks;
 namespace SuperbowlTests
 {
     [TestFixture]
-    public class Class1
+    public class SuperbowlDateTests
     {
+        [Test]
+        public void Entering_the_number_1_and_convert_To_Roman_Numerals()
+        {
+            //Arrange
+            var  number = 1;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var resuot = 
+
+            //Assert
+        }
     }
+
+    
 }
