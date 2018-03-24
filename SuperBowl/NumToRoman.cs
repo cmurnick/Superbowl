ab241a179bb45059171860e23048919dd71994e2
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-03-23 20:41:26 -0500

    update

--- SuperBowl/NumToRoman.cs
+++ SuperBowl/NumToRoman.cs
@@ -1,9 +1,57 @@
-﻿namespace SuperbowlTests
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Superbowl
 {
-    internal class NumToRoman
+    public class NumToRoman
     {
-        public NumToRoman()
+   
+
+
+        public string Ashley(int number)
+
         {
+            //int parsedNumber = int.Parse(number);
+
+            string[] romanNumerals = new string[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        int[] decimals = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        string romanNumber = string.Empty;
+
+ 
+
+            for (int i = 0; i<romanNumerals.Length; i++)
+
+            {
+
+                while (number >= decimals[i])
+
+                {
+
+                    number -= decimals[i];
+
+                    romanNumber += romanNumerals[i];
+                    if (number == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            return romanNumber;
+            //Console.WriteLine(romanNumber);
+
+            //Console.ReadLine();
+
         }
-    }
+      }
+
+
+
+
+
+    //public class Number
+    //    {
+    //        public int NumberToConvert { get; set; }
+    //    }
 }
--- SuperbowlDates/SuperbowlDateTests.cs
+++ SuperbowlDates/SuperbowlDateTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using Superbowl;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,56 @@ namespace SuperbowlTests
             var numToRoman = new NumToRoman();
 
             //Act
-            var resuot = 
+            var result = numToRoman.Ashley(number);
 
             //Assert
+            Assert.AreEqual("I", result );
+
+            
+        }
+
+        [Test]
+        public void Entering_the_number_4_and_convert_To_Roman_Numerals()
+        {
+            //Arrange
+            var number = 5;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var result = numToRoman.Ashley(number);
+
+            //Assert
+            Assert.AreEqual("V", result);
+
+
+        }
+
+        [Test]
+        public void Entering_the_number_9_and_convert_To_Roman_Numerals()
+        {
+            //Arrange
+            var number = 9;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var result = numToRoman.Ashley(number);
+
+            //Assert
+            Assert.AreEqual("IX", result);
+        }
+
+        [Test]
+        public void Entering_the_number_big_and_convert_To_Roman_Numerals()
+        {
+            //Arrange
+            int number = 500;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var result = numToRoman.Ashley(number);
+
+            //Assert
+            Assert.AreEqual("D", result);
         }
     }
 
